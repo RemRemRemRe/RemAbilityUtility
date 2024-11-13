d467d4792da4d1162bb30e65cc3c84e25bb1007d
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2024-11-14 00:28:50 +0800

    Changed: move FRemTagEventManager to separate plugin for better dependency handling

---
+++ Source/RemAbilityUtility/Private/RemTagEventManager.cpp
@@ -0,0 +1,85 @@
+﻿// Copyright RemRemRemRe. 2024. All Rights Reserved.
+
+
+#include "RemTagEventManager.h"
+
+#include "AbilitySystemComponent.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(RemTagEventManager)
+
+FRemTagEventManager::FRemTagEventManager(UAbilitySystemComponent* InAbilitySystem)
+	: AbilitySystem(InAbilitySystem)
+{
+}
+
+void FRemTagEventManager::RegisterGameplayTagEventUnique(const FGameplayTag Tag, const EGameplayTagEventType::Type EventType,
+	const FDelegate& Delegate)
+{
+	if (!TagToEventHandle.Contains(Tag))
+	{
+		const auto Handle = AbilitySystem->RegisterGameplayTagEvent(Tag, EventType).Add(Delegate);
+
+		TagToEventHandle.Add(Tag, Handle);
+	}
+}
+
+void FRemTagEventManager::RegisterGameplayTagEventUnique(const FGameplayTag Tag, const FDelegate& Delegate)
+{
+	return RegisterGameplayTagEventUnique(Tag, EGameplayTagEventType::NewOrRemoved, Delegate);
+}
+
+void FRemTagEventManager::RegisterGameplayTagEvent(const FGameplayTag Tag, const EGameplayTagEventType::Type EventType,
+	const FDelegate& Delegate)
+{
+	const auto Handle = AbilitySystem->RegisterGameplayTagEvent(Tag, EventType).Add(Delegate);
+
+	TagToEventHandle.FindOrAdd(Tag, Handle);
+}
+
+void FRemTagEventManager::RegisterGameplayTagEvent(const FGameplayTag Tag,
+	const FDelegate& Delegate)
+{
+	return RegisterGameplayTagEvent(Tag, EGameplayTagEventType::NewOrRemoved, Delegate);
+}
+
+void FRemTagEventManager::Reset()
+{
+	SetAbilitySystem(nullptr);
+}
+
+void FRemTagEventManager::UnRegisterGameplayTagEvents()
+{
+	if (AbilitySystem.IsValid())
+	{
+		for (const auto& Data : TagToEventHandle)
+		{
+			AbilitySystem->RegisterGameplayTagEvent(Data.Key).Remove(Data.Value);
+		}
+	}
+
+	TagToEventHandle.Reset();
+}
+
+bool FRemTagEventManager::IsValid() const
+{
+	return AbilitySystem.IsValid();
+}
+
+FRemTagEventManager::~FRemTagEventManager()
+{
+	UnRegisterGameplayTagEvents();
+}
+
+bool FRemTagEventManager::SetAbilitySystem(UAbilitySystemComponent* InAbilitySystem)
+{
+	if (InAbilitySystem != AbilitySystem)
+	{
+		UnRegisterGameplayTagEvents();
+
+		AbilitySystem = InAbilitySystem;
+
+		return true;
+	}
+
+	return false;
+}
---
+++ Source/RemAbilityUtility/Public/RemTagEventManager.h
@@ -0,0 +1,57 @@
+﻿// Copyright RemRemRemRe. 2024. All Rights Reserved.
+
+#pragma once
+
+#include "GameplayTagContainer.h"
+#include "Delegates/DelegateSignatureImpl.inl"
+#include "Macro/RemGenerateMembersMacro.h"
+
+#include "RemTagEventManager.generated.h"
+
+namespace EGameplayTagEventType
+{
+	enum Type : int;
+}
+
+class UAbilitySystemComponent;
+
+USTRUCT(BlueprintType)
+struct REMABILITYUTILITY_API FRemTagEventManager
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Rem", meta = (AllowPrivateAccess))
+	TWeakObjectPtr<UAbilitySystemComponent> AbilitySystem{};
+
+	TMap<FGameplayTag, FDelegateHandle> TagToEventHandle{};
+
+public:
+	explicit FRemTagEventManager(UAbilitySystemComponent* InAbilitySystem = nullptr);
+
+	~FRemTagEventManager();
+
+	using FDelegate = TMulticastDelegate<void(const FGameplayTag, int32)>::FDelegate;
+
+	/**
+	 * @return true if AbilitySystem changed
+	 */
+	bool SetAbilitySystem(UAbilitySystemComponent* InAbilitySystem);
+	REM_DEFINE_CONST_ONLY_GETTERS_RETURN_REFERENCE_SIMPLE(AbilitySystem)
+	REM_DEFINE_CONST_ONLY_GETTERS_RETURN_REFERENCE_SIMPLE(TagToEventHandle)
+
+	// won't override events if bound
+	void RegisterGameplayTagEventUnique(FGameplayTag Tag, EGameplayTagEventType::Type EventType, const FDelegate& Delegate);
+	void RegisterGameplayTagEventUnique(FGameplayTag Tag, const FDelegate& Delegate);
+
+	// will override events if bound
+	void RegisterGameplayTagEvent(FGameplayTag Tag, EGameplayTagEventType::Type EventType, const FDelegate& Delegate);
+	void RegisterGameplayTagEvent(FGameplayTag Tag, const FDelegate& Delegate);
+
+	void Reset();
+
+	bool IsValid() const;
+
+protected:
+	void UnRegisterGameplayTagEvents();
+};
