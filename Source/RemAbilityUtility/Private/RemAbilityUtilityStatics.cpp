cff7c7172d910dbd6363fc6019fa99a659f130ec
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-06-08 01:33:50 +0800

    New: API HasAnyMatchingQueryTag, HasAnyMatchingTags for array view

--- Source/RemAbilityUtility/Private/RemAbilityUtilityStatics.cpp
+++ Source/RemAbilityUtility/Private/RemAbilityUtilityStatics.cpp
@@ -3,5 +3,54 @@
 
 #include "RemAbilityUtilityStatics.h"
 
+#include "AbilitySystemComponent.h"
+#include "GameplayTagContainer.h"
+
 #include UE_INLINE_GENERATED_CPP_BY_NAME(RemAbilityUtilityStatics)
 
+bool Rem::Ability::HasAnyMatchingQueryTag(const UAbilitySystemComponent& AbilitySystem, const FGameplayTagQuery& TagQuery)
+{
+	return HasAnyMatchingTags(AbilitySystem, TagQuery.GetGameplayTagArray());
+}
+
+bool Rem::Ability::HasAllMatchingQueryTag(const UAbilitySystemComponent& AbilitySystem,
+	const FGameplayTagQuery& TagQuery)
+{
+	return HasAllMatchingTags(AbilitySystem, TagQuery.GetGameplayTagArray());
+}
+
+bool Rem::Ability::HasAnyMatchingTags(const UAbilitySystemComponent& AbilitySystem, const TConstArrayView<FGameplayTag> Tags)
+{
+	if (Tags.IsEmpty())
+	{
+		return true;
+	}
+
+	for (auto& Tag : Tags)
+	{
+		if (AbilitySystem.GetTagCount(Tag) > 0)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Rem::Ability::HasAllMatchingTags(const UAbilitySystemComponent& AbilitySystem, const TConstArrayView<FGameplayTag> Tags)
+{
+	if (Tags.IsEmpty())
+	{
+		return true;
+	}
+
+	for (auto& Tag : Tags)
+	{
+		if (AbilitySystem.GetTagCount(Tag) == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
--- Source/RemAbilityUtility/Public/RemAbilityUtilityStatics.h
+++ Source/RemAbilityUtility/Public/RemAbilityUtilityStatics.h
@@ -5,6 +5,10 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "RemAbilityUtilityStatics.generated.h"
 
+struct FGameplayTag;
+struct FGameplayTagQuery;
+class UAbilitySystemComponent;
+
 UCLASS()
 class REMABILITYUTILITY_API URemAbilityUtilityStatics : public UBlueprintFunctionLibrary
 {
@@ -15,5 +19,9 @@ public:
 
 namespace Rem::Ability
 {
+	REMABILITYUTILITY_API bool HasAnyMatchingQueryTag(const UAbilitySystemComponent& AbilitySystem, const FGameplayTagQuery& TagQuery);
+	REMABILITYUTILITY_API bool HasAllMatchingQueryTag(const UAbilitySystemComponent& AbilitySystem, const FGameplayTagQuery& TagQuery);
 
+	REMABILITYUTILITY_API bool HasAnyMatchingTags(const UAbilitySystemComponent& AbilitySystem, TConstArrayView<FGameplayTag> Tags);
+	REMABILITYUTILITY_API bool HasAllMatchingTags(const UAbilitySystemComponent& AbilitySystem, TConstArrayView<FGameplayTag> Tags);
 }
