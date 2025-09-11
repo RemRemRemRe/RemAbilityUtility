b0bf70ccf635a9bced2f2b790868a6ae0f3e2a6d
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2025-09-12 02:14:28 +0800

    Changed: adopt REM_API style

--- Source/RemAbilityUtility/Public/RemAbilityUtilityLog.h
+++ Source/RemAbilityUtility/Public/RemAbilityUtilityLog.h
@@ -3,5 +3,10 @@
 #pragma once
 
 #include "Logging/LogMacros.h"
+#include "Macro/RemMacroUtilities.h"
 
-REMABILITYUTILITY_API DECLARE_LOG_CATEGORY_EXTERN(LogRemAbilityUtility, Log, All);
+#define REM_API REMABILITYUTILITY_API
+
+REM_API DECLARE_LOG_CATEGORY_EXTERN(LogRemAbilityUtility, REM_DEFAULT_LOG_VERBOSITY, REM_MAX_LOG_VERBOSITY)
+
+#undef REM_API
