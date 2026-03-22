eefc3c71c24ca7afafa3def3b3de2c27a622033b
RemRemRemRe <RemRemRemRe@users.noreply.github.com>
2026-03-23 00:50:08 +0800

    New: introduce RemSharedModuleRules to avoid duplication work

--- Source/RemAbilityUtility/RemAbilityUtility.Build.cs
+++ Source/RemAbilityUtility/RemAbilityUtility.Build.cs
@@ -1,19 +1,13 @@
 // Copyright RemRemRemRe. 2025. All Rights Reserved.
 
 using UnrealBuildTool;
+using Rem.BuildRule;
 
 public class RemAbilityUtility : ModuleRules
 {
 	public RemAbilityUtility(ReadOnlyTargetRules target) : base(target)
 	{
-		CppCompileWarningSettings.ShadowVariableWarningLevel = WarningLevel.Error;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
-		DefaultBuildSettings = BuildSettingsVersion.Latest;
-		CppStandard = CppStandardVersion.EngineDefault;
-		CppCompileWarningSettings.UnsafeTypeCastWarningLevel = WarningLevel.Warning;
-
-		CppCompileWarningSettings.NonInlinedGenCppWarningLevel = WarningLevel.Warning;
-		bUseUnity = false;
+        RemSharedModuleRules.Apply(this);
 
 		PrivateDependencyModuleNames.AddRange(
 			[
